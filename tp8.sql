4464422b86787ef04b17df84cfc71c3da8a94442
Juan Cruz Rodriguez <juancruzrodriguez0@gmail.com>
2025-09-21 22:00:29 -0300

    Agregué datos
    1 cliente, 4 mascotas, 3 consultas y 3 visitas

--- tp8.sql
+++ tp8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-09-2025 a las 02:25:12
+-- Tiempo de generación: 22-09-2025 a las 02:59:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,6 +40,13 @@ CREATE TABLE `clientes` (
   `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_Cliente`, `dni`, `nombre`, `apellido`, `telefono`, `direccion`, `contacto_alternativo`, `activo`) VALUES
+(1, 42357107, 'Juan Cruz', 'Rodriguez', 2664572404, 'Licitación, 2 Manzana 116, Casa 02', 2664001122, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +65,16 @@ CREATE TABLE `mascota` (
   `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `mascota`
+--
+
+INSERT INTO `mascota` (`id_Mascota`, `alias`, `sexo`, `especie`, `raza`, `color_Pelo`, `fecha_Nac`, `id_Cliente`, `activo`) VALUES
+(1, 'Camilo', 'Macho', 'Perro', 'Caniche', 'Blanco', '2011-03-18', 1, 1),
+(2, 'Frida', 'Hembra', 'Perro', 'Callejero', 'Negro', '2024-04-22', 1, 1),
+(3, 'Margarita', 'Hembra', 'Tortuga', '-', '-', '2006-11-08', 1, 1),
+(4, 'Bartolo', 'Macho', 'Tortuga', '-', '-', '2010-07-29', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -72,6 +89,15 @@ CREATE TABLE `tratamiento` (
   `tipo_Tratamiento` varchar(30) NOT NULL,
   `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tratamiento`
+--
+
+INSERT INTO `tratamiento` (`id_Tratamiento`, `descripcion`, `medicamento`, `importe`, `tipo_Tratamiento`, `activo`) VALUES
+(1, 'Control', '-', 12500, 'Control', 1),
+(2, 'Control', '-', 13000, 'Control', 1),
+(3, 'Mordida de perro en el cuello', 'Clorhexidina', 15000, 'Desinfección de la zona', 1);
 
 -- --------------------------------------------------------
 
@@ -88,6 +114,15 @@ CREATE TABLE `visita` (
   `id_Tratamiento` int(11) NOT NULL,
   `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `visita`
+--
+
+INSERT INTO `visita` (`id_Visita`, `id_Mascota`, `fecha_Visita`, `detalle`, `peso`, `id_Tratamiento`, `activo`) VALUES
+(1, 1, '2025-09-18', 'Control', 12.4, 1, 1),
+(2, 2, '2025-09-18', 'Control', 4.6, 2, 1),
+(3, 1, '2025-09-20', 'Mordida Perro', 12.4, 3, 1);
 
 --
 -- Índices para tablas volcadas
@@ -128,25 +163,25 @@ ALTER TABLE `visita`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_Cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  MODIFY `id_Mascota` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tratamiento`
 --
 ALTER TABLE `tratamiento`
-  MODIFY `id_Tratamiento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Tratamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `visita`
 --
 ALTER TABLE `visita`
-  MODIFY `id_Visita` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_Visita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
