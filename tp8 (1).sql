adf44ee97f237a55934de53cd91b64c461537528
jeremiassflores <jeremiassflores@gmail.com>
2025-09-22 20:47:07 -0300

    Add files via upload
    agregue 1 dato mas

---
+++ tp8 (1).sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-09-2025 a las 01:44:45
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tp8`
+--
+CREATE DATABASE IF NOT EXISTS `tp8` DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+USE `tp8`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_Cliente` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `contacto_alternativo` bigint(20) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_Cliente`, `dni`, `nombre`, `apellido`, `telefono`, `direccion`, `contacto_alternativo`, `activo`) VALUES
+(1, 42357107, 'Juan Cruz', 'Rodriguez', 2664572404, 'Licitación, 2 Manzana 116, Casa 02', 2664001122, 1),
+(2, 38751080, 'Jeremias', 'Sirur Flores', 2664634076, 'B114M189C18', 2664787978, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascota`
+--
+
+CREATE TABLE `mascota` (
+  `id_Mascota` int(11) NOT NULL,
+  `alias` varchar(30) NOT NULL,
+  `sexo` varchar(30) NOT NULL,
+  `especie` varchar(30) NOT NULL,
+  `raza` varchar(30) NOT NULL,
+  `color_Pelo` varchar(30) NOT NULL,
+  `fecha_Nac` date NOT NULL,
+  `id_Cliente` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `mascota`
+--
+
+INSERT INTO `mascota` (`id_Mascota`, `alias`, `sexo`, `especie`, `raza`, `color_Pelo`, `fecha_Nac`, `id_Cliente`, `activo`) VALUES
+(1, 'Camilo', 'Macho', 'Perro', 'Caniche', 'Blanco', '2011-03-18', 1, 1),
+(2, 'Frida', 'Hembra', 'Perro', 'Callejero', 'Negro', '2024-04-22', 1, 1),
+(3, 'Margarita', 'Hembra', 'Tortuga', '-', '-', '2006-11-08', 1, 1),
+(4, 'Bartolo', 'Macho', 'Tortuga', '-', '-', '2010-07-29', 1, 1),
+(5, 'Rolo', 'Macho', 'Perro', 'Labrador', 'Negro', '2022-04-17', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `id_Tratamiento` int(11) NOT NULL,
+  `descripcion` varchar(70) NOT NULL,
+  `medicamento` varchar(50) NOT NULL,
+  `importe` double NOT NULL,
+  `tipo_Tratamiento` varchar(30) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tratamiento`
+--
+
+INSERT INTO `tratamiento` (`id_Tratamiento`, `descripcion`, `medicamento`, `importe`, `tipo_Tratamiento`, `activo`) VALUES
+(1, 'Control', '-', 12500, 'Control', 1),
+(2, 'Control', '-', 13000, 'Control', 1),
+(3, 'Mordida de perro en el cuello', 'Clorhexidina', 15000, 'Desinfección de la zona', 1),
+(4, 'Dermatitis leve', 'Platsul c/ 8hs', 32000, 'Aplicacion de crema', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visita`
+--
+
+CREATE TABLE `visita` (
+  `id_Visita` int(11) NOT NULL,
+  `id_Mascota` int(11) NOT NULL,
+  `fecha_Visita` date NOT NULL,
+  `detalle` varchar(50) NOT NULL,
+  `peso` double NOT NULL,
+  `id_Tratamiento` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `visita`
+--
+
+INSERT INTO `visita` (`id_Visita`, `id_Mascota`, `fecha_Visita`, `detalle`, `peso`, `id_Tratamiento`, `activo`) VALUES
+(1, 1, '2025-09-18', 'Control', 12.4, 1, 1),
+(2, 2, '2025-09-18', 'Control', 4.6, 2, 1),
+(3, 1, '2025-09-20', 'Mordida Perro', 12.4, 3, 1),
+(4, 5, '2025-09-22', 'Revision proxima semana', 21, 4, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_Cliente`);
+
+--
+-- Indices de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD PRIMARY KEY (`id_Mascota`),
+  ADD KEY `id_Cliente` (`id_Cliente`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`id_Tratamiento`);
+
+--
+-- Indices de la tabla `visita`
+--
+ALTER TABLE `visita`
+  ADD PRIMARY KEY (`id_Visita`),
+  ADD KEY `id_Tratamiento` (`id_Tratamiento`),
+  ADD KEY `id_Mascota` (`id_Mascota`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  MODIFY `id_Mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `id_Tratamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `visita`
+--
+ALTER TABLE `visita`
+  MODIFY `id_Visita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`id_Cliente`) REFERENCES `clientes` (`id_Cliente`);
+
+--
+-- Filtros para la tabla `visita`
+--
+ALTER TABLE `visita`
+  ADD CONSTRAINT `visita_ibfk_1` FOREIGN KEY (`id_Mascota`) REFERENCES `mascota` (`id_Mascota`),
+  ADD CONSTRAINT `visita_ibfk_2` FOREIGN KEY (`id_Tratamiento`) REFERENCES `tratamiento` (`id_Tratamiento`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
